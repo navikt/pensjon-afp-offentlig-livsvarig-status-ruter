5bde2ec06dbc891220c56746f52db2ea6ecc4aac
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2024-02-19 12:48:32 +0100

    Opprettet mock endepunkt
    Co-authored-by: Per Christian Moen <per.christian.moen@nav.no>

---
+++ src/main/kotlin/no/nav/pensjon/afp_offentlig/afp_offentlig_livsvarig_status_ruter/AfpOffentligLivsvarigStautsRuterController.kt
@@ -0,0 +1,46 @@
+package no.nav.pensjon.afp_offentlig.afp_offentlig_livsvarig_status_ruter
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+
+@RestController
+@RequestMapping("/api/afp-offentlig-status")
+class AfpOffentligLivsvarigStautsRuterController(
+) {
+
+    @PostMapping("/hent")
+    fun hentAfpOffentligStatus(@RequestBody request: HentStatusRequest): HentStatusResponse {
+        return HentStatusResponse(null,null,null)
+    }
+    data class HentStatusRequest(
+        val fnr: String,
+        val onsketVirkningtidspunkt: LocalDate,
+        val hjemmel: String,
+        val tema: String,
+        val type: String,
+    )
+    data class HentStatusResponse(
+        val innvilget: InnvilgetDto?,
+        val soknad: SoknadDto?,
+        val manglendeApi: ManglendeApiDto?,
+    )
+
+    data class InnvilgetDto(
+        val belop: Int,
+        val tpNummer: Int,
+        val startdato: LocalDate,
+        val sluttdato: LocalDate?,
+        val sistRegulert: LocalDate,
+    )
+
+    data class SoknadDto(
+        val onsketVirkningsdato: LocalDate,
+    )
+
+    data class ManglendeApiDto(
+        val onsketVirkningsdato: LocalDate
+    )
+}
