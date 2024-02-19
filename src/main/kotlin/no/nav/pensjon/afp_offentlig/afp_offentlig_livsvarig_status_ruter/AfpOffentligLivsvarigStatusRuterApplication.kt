e638528a65578b720ab9e66ee9c8c269d9133bd2
Hans Magnus Hallaraker <hans.magnus.hallaraker@nav.no>
2024-02-19 11:31:47 +0100

    Tom app, generert med Spring Initializr

---
+++ src/main/kotlin/no/nav/pensjon/afp_offentlig/afp_offentlig_livsvarig_status_ruter/AfpOffentligLivsvarigStatusRuterApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.afp_offentlig.afp_offentlig_livsvarig_status_ruter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AfpOffentligLivsvarigStatusRuterApplication
+
+fun main(args: Array<String>) {
+	runApplication<AfpOffentligLivsvarigStatusRuterApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/pensjon/afp_offentlig/afp_offentlig_livsvarig_status_ruter/AfpOffentligLivsvarigStatusRuterApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pensjon.afp_offentlig.afp_offentlig_livsvarig_status_ruter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AfpOffentligLivsvarigStatusRuterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
