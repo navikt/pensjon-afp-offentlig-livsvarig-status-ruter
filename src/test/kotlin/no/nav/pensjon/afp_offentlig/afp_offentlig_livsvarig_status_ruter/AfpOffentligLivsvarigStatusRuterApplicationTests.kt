0e8182fd2347293619915add3f8371f2da25a455
Per Christian Moen <per.christian.moen@nav.no>
2024-02-21 14:56:01 +0100

    Bruker local profil for test
    Co-authored-by: Hans Magnus Hallaråker <hans.magnus.hallaraker@nav.no>

--- src/test/kotlin/no/nav/pensjon/afp_offentlig/afp_offentlig_livsvarig_status_ruter/AfpOffentligLivsvarigStatusRuterApplicationTests.kt
+++ src/test/kotlin/no/nav/pensjon/afp_offentlig/afp_offentlig_livsvarig_status_ruter/AfpOffentligLivsvarigStatusRuterApplicationTests.kt
@@ -2,8 +2,10 @@ package no.nav.pensjon.afp_offentlig.afp_offentlig_livsvarig_status_ruter
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AfpOffentligLivsvarigStatusRuterApplicationTests {
 
 	@Test
